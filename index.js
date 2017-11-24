e5d616b8a07ed1ba591aa1454393f841caf96c9e
Nick Garcia <89nickgarcia@gmail.com>
2017-11-24 19:09:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name){
+  return kittens.push('Ralph');
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift('Bob');
+}
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop('Garfield');
+}
+
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift('Milo');
+}
+
+function appendKitten(name){
+  var newkitten = [kittens,...'Broom'];
+  return newkitten;
+}
